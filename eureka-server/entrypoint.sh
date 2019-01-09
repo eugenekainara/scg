435fbdfcd40ea58611d676493a7980fe784ffeb2
Rostyslav Reznichenko <Rostislav.Reznichenko@HHSTechGroup.com>
2019-01-09 13:14:34 +0200

    linked services

--- eureka-server/entrypoint.sh
+++ eureka-server/entrypoint.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+for i in `echo $LINKED_SERVICES | tr "," " "`
+do
+  until 2>/dev/null < /dev/tcp/${i%%:*}/${i##*:}
+  do
+    sleep 1s
+    echo "waiting for $i"
+  done
+  echo "connected to $i"
+done
+
+echo "Connected to $LINKED_SERVICES"
+
 exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Djava.security.egd=file:/dev/./urandom -jar /eureka-server.jar
--- sample-application/entrypoint.sh
+++ sample-application/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-sleep 10
+for i in `echo $LINKED_SERVICES | tr "," " "`
+do
+  until 2>/dev/null < /dev/tcp/${i%%:*}/${i##*:}
+  do
+    sleep 1s
+    echo "waiting for $i"
+  done
+  echo "connected to $i"
+done
+
+echo "Connected to $LINKED_SERVICES"
+
 exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Djava.security.egd=file:/dev/./urandom -jar /sample-application.jar
