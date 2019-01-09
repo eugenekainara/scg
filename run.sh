f223cd856b363183135c4bd9c34e5bf7cd2ea831
Rostyslav Reznichenko <Rostislav.Reznichenko@HHSTechGroup.com>
2019-01-09 17:12:55 +0200

    1141

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ EUREKA_PID=$!
 java -jar sample-application/build/libs/sample-application*.jar &>/dev/null &
 SAMPLE_PID=$!
 
-java -jar gateway-server/build/libs/gateway-server*.jar &>/dev/null &
+java -jar gateway-server/build/libs/gateway-server*.jar > log &
 GATEWAY_PID=$!
 
 echo "wait for initialization"
-until curl "http://localhost:8881/sample/tests" -w %{http_code}\\n -s -o /dev/null -L --insecure -f || [[ "$retries" -lt 0 ]];
+until curl "http://localhost:8881/sample/tests" -w %{http_code}\\n -s -o /dev/null -L --insecure | grep -E "(200|500)" || [[ "$retries" -lt 0 ]];
 do
 sleep 5s
 done
--- sample-application/src/test/java/com/example/TestControllerTest.java
+++ sample-application/src/test/java/com/example/TestControllerTest.java
@@ -2,21 +2,36 @@ package com.example;
 
 import org.junit.Test;
 import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 public class TestControllerTest {
 
-    private RestTemplate restTemplate = new RestTemplate();
-
     @Test
     public void getTest() {
-        for (int i = 0; i < 100; i++) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+
+            }
+        });
+
+        for (int i = 0; i < 10; i++) {
             try {
                 Thread.sleep(100);
             } catch (InterruptedException ignored) {
             }
             System.out.println("Iteration - " + i);
-            ResponseEntity<Object> forEntity = restTemplate.getForEntity("http://localhost:8881/sample/tests", Object.class);
+            ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:8881/sample/tests", String.class);
             System.out.println("Status code - " + forEntity.getStatusCode());
         }
     }
