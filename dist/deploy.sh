01e78cfc7b379d9ffe7a9365d274e2b08c2ca6d4
Rostyslav Reznichenko <Rostyslav.Reznichenko@solve.care>
2019-05-17 11:46:21 +0300

    fluentbit

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 BASEDIR=$(pwd)
-
+docker stack rm poc
+#sleep 10
 set -a
 if [ -e $BASEDIR/app.properties ]; then
 source $BASEDIR/app.properties
@@ -10,9 +11,9 @@ docker stack deploy -c docker-compose-stack-single.yml --with-registry-auth poc
 sleep 20s
 
 echo "Waiting till First and Second are up http://${DOMAIN:-localhost}"
-urls="http://${DOMAIN:-localhost}/first/tests"
+urls="http://${DOMAIN:-localhost}:8883/first/tests"
 
-retries=120
+retries=30
 for i in $urls
 do
   until curl "$i" -w %{http_code}\\n -fkLs -o /dev/null || [[ "$retries" -lt 0 ]];
